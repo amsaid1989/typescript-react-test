c9f3caa27e56421c42ecfe1ba74a610ecb7bcaa6
Abdelrahman <said.abdelrahman89@gmail.com>
2021-12-12 22:09:06 +0000

    Completed a very basic todo app with React and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+import CompTodoList from "./CompTodoList";
+import { Todo, TodoList } from "./interfaces";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState("");
+    const [todos, setTodos] = useState<TodoList | null>(null);
+
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (buttonRef && buttonRef.current) {
+            buttonRef.current.disabled = text.length === 0;
+        }
+
+        setText(e.target.value);
+    };
+
+    const addTodo: React.MouseEventHandler<HTMLButtonElement> = () => {
+        const num = todos ? todos.length + 1 : 0;
+
+        const todo: Todo = {
+            id: `${num}-${text}`,
+            text: text,
+            completed: false,
+        };
+
+        const todoList: TodoList = todos ? [...todos, todo] : [todo];
+
+        setTodos(todoList);
+
+        setText("");
+    };
+
+    const deleteTodo = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        const id = (e.target as HTMLButtonElement).id;
+
+        if (todos) {
+            const todoList: TodoList = [...todos].filter(
+                (todo) => todo.id !== id
+            );
+
+            setTodos(todoList);
+        }
+    };
+
+    const toggleTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const id = (e.target as HTMLInputElement).id;
+
+        if (todos) {
+            const todoList: TodoList = [...todos].map((todo) => {
+                if (todo.id === id) {
+                    todo.completed = e.target.checked;
+
+                    return todo;
+                }
+
+                return todo;
+            });
+
+            setTodos(todoList);
+        }
+    };
+
+    return (
+        <main className="App">
+            <section id="todo-add">
+                <input
+                    id="todo-text"
+                    value={text}
+                    onChange={handleTextChange}
+                />
+                <button
+                    ref={buttonRef}
+                    onClick={addTodo}
+                    disabled={text.length === 0}
+                >
+                    Add todo
+                </button>
+            </section>
+
+            {todos && (
+                <CompTodoList
+                    todos={todos}
+                    changeHandler={toggleTodo}
+                    deleteHandler={deleteTodo}
+                />
+            )}
+        </main>
+    );
 }
 
 export default App;
---
+++ src/CompTodo.tsx
@@ -0,0 +1,27 @@
+import "./CompTodo.css";
+import { Todo } from "./interfaces";
+
+type CompTodoProps = {
+    todo: Todo;
+    changeHandler: React.ChangeEventHandler<HTMLInputElement>;
+    deleteHandler: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+function CompTodo(props: CompTodoProps): JSX.Element {
+    return (
+        <div className="todo-item">
+            <input
+                id={props.todo.id}
+                type="checkbox"
+                checked={props.todo.completed}
+                onChange={props.changeHandler}
+            />
+            <span>{props.todo.text}</span>
+            <button id={props.todo.id} onClick={props.deleteHandler}>
+                Delete
+            </button>
+        </div>
+    );
+}
+
+export default CompTodo;
